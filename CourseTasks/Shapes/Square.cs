eaafaffc787c2b0704e984a16ce80b723831048e
alexandergubin <gubin.alexander88@gmail.com>
2018-05-24 21:32:57 +0600

    create Project

---
+++ CourseTasks/Shapes/Square.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Square : IShape
+    {
+        private double heigh;
+        private double width;
+
+        public Square(double side)
+        {
+            heigh = side;
+            width = side;
+        }
+
+        public double GetArea()
+        {
+            return heigh * width;
+        }
+
+        public double GetHeight()
+        {
+            return heigh;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * (heigh + width);
+        }
+
+        public double GetWidth()
+        {
+            return width;
+        }
+    }
+}
