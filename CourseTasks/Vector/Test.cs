5140a59d4e9563549f83c32211b2715a896a55a0
alexandergubin <gubin.alexander88@gmail.com>
2018-05-25 20:26:29 +0600

    fix bugs

--- CourseTasks/Vector/Test.cs
+++ CourseTasks/Vector/Test.cs
@@ -10,18 +10,19 @@ namespace Vector
     {
         public static void Main(string[] args)
         {
-            double[] array1 = { 1.0, 2.0 };
+            double[] array1 = { 1.0, 2.0, 3.0 };
             double[] array2 = { 1.0, 2.0 };
+            double[] array3 = { 1.0 };
             Vector vector1 = new Vector(array1);
             Vector vector2 = new Vector(array2);
 
             Console.WriteLine("vector1 = " + vector1.ToString());
 
+            vector2.Add(vector1);
+            Console.WriteLine("vector2 + vector1 = " + vector2);
+
             vector1.Multiply(2);
             Console.WriteLine("vector1 * 2 = " + vector1.ToString());
-
-            vector1.Add(vector2);
-            Console.WriteLine("vector1 + vector2 = " + vector1);
 
             vector1.Sub(vector1);
             Console.WriteLine("vector1 - vector1 = " + vector1.ToString());
@@ -32,8 +33,8 @@ namespace Vector
             Console.WriteLine();
             Console.WriteLine("vector2 = " + vector2.ToString());
 
-            vector1 = Vector.Add(vector2, vector2);
-            Console.WriteLine("vector1 = Vector.Add(vector2, vector2) = " + vector1.ToString());
+            vector1 = Vector.Sub(new Vector(array3), vector2);
+            Console.WriteLine("vector1 = Vector.Add({1.0}, vector2) = " + vector1.ToString());
 
             double composition = Vector.ScalarMultiply(vector1, vector2);
             Console.WriteLine("Vector.ScalarMultiply(vector1, vector2) = " + composition);
--- CourseTasks/Vector/Vector.cs
+++ CourseTasks/Vector/Vector.cs
@@ -18,49 +18,68 @@ namespace Vector
         {
             if (n <= 0)
             {
-                throw new ArgumentException("components.Length must be > 0");
+                throw new ArgumentException("длина вектора должна быть больше 0");
             }
             components = new double[n];
         }
 
         public Vector(Vector other)
         {
+            if (ReferenceEquals(other, null))
+            {
+                throw new ArgumentNullException("в параметры конструктора передан null");
+            }
             components = new double[other.components.Length];
             Array.Copy(other.components, components, other.components.Length);
         }
 
         public Vector(double[] array)
         {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("длина вектора должна быть больше 0");
+            }
             components = new double[array.Length];
             Array.Copy(array, components, array.Length);
         }
 
         public Vector(int n, double[] array)
         {
+            if (ReferenceEquals(array, null))
+            {
+                throw new NullReferenceException("массив равен null");
+            }
             if (n <= 0)
             {
-                throw new ArgumentException("components.Length must be > 0");
+                throw new ArgumentException("размерность вектора не может быть меньше длинны передаваемого массива");
             }
             components = new double[n];
-            Array.Copy(array, components, array.Length);
+            Array.Copy(array, components, (n < array.Length) ? n : array.Length);
         }
 
         public override string ToString()
         {
-            String line = "{";
+            System.Text.StringBuilder sb = new System.Text.StringBuilder("{");
             for (int i = 0; i < components.Length; i++)
             {
-                line += string.Format(" {0:f}", components[i]);
+                sb.AppendFormat(" {0:f}", components[i]);
                 if (i < components.Length - 1)
                 {
-                    line += ",";
+                    sb.Append(",");
                 }
             }
-            return line + " }";
+            sb.Append(" }");
+            return sb.ToString();
         }
 
         public void Add(Vector other)
         {
+            if (Size < other.Size)
+            {
+                double[] newComponents = new double[other.Size];
+                Array.Copy(components, newComponents, components.Length);
+                components = newComponents;
+            }
             for (int i = 0; i < components.Length; i++)
             {
                 if (i < other.components.Length)
@@ -72,6 +91,12 @@ namespace Vector
 
         public void Sub(Vector other)
         {
+            if (Size < other.Size)
+            {
+                double[] newComponents = new double[other.Size];
+                Array.Copy(components, newComponents, components.Length);
+                components = newComponents;
+            }
             for (int i = 0; i < components.Length; i++)
             {
                 if (i < other.components.Length)
@@ -99,9 +124,9 @@ namespace Vector
             get
             {
                 double length = 0;
-                for (int i = 0; i < components.Length; i++)
+                foreach (double e in components)
                 {
-                    length += Math.Pow(components[i], 2.0);
+                    length += Math.Pow(e, 2.0);
                 }
                 return Math.Sqrt(length);
             }
@@ -128,7 +153,7 @@ namespace Vector
                 return false;
             }
             Vector v = (Vector)obj;
-            if (obj.GetHashCode() != this.GetHashCode() || v.components.Length != components.Length)
+            if (v.components.Length != components.Length)
             {
                 return false;
             }
@@ -149,7 +174,13 @@ namespace Vector
         {
             int prime = 37;
             int hash = 1;
-            hash = hash * prime + (ReferenceEquals(components, null) ? components.GetHashCode() : 0);
+            if (!ReferenceEquals(components, null))
+            {
+                foreach (double e in components)
+                {
+                    hash = hash * prime + e.GetHashCode();
+                }
+            }
             hash = hash * prime + Size.GetHashCode();
             hash = hash * prime + Length.GetHashCode();
             return hash;
@@ -159,18 +190,14 @@ namespace Vector
 
         public static Vector Add(Vector vector1, Vector vector2)
         {
-            int maxLength = Math.Max(vector1.Size, vector2.Size);
-            Vector resultVector = new Vector(maxLength);
-            resultVector.Add(vector1);
+            Vector resultVector = new Vector(vector1);
             resultVector.Add(vector2);
             return resultVector;
         }
 
         public static Vector Sub(Vector vector1, Vector vector2)
         {
-            int maxLength = Math.Max(vector1.Size, vector2.Size);
-            Vector resultVector = new Vector(maxLength);
-            resultVector.Add(vector1);
+            Vector resultVector = new Vector(vector1);
             resultVector.Sub(vector2);
             return resultVector;
         }
@@ -178,11 +205,10 @@ namespace Vector
         public static double ScalarMultiply(Vector vector1, Vector vector2)
         {
             double result = 0;
-            int i = 0;
-            while (i < vector1.Size && i < vector2.Size)
+            int border = Math.Min(vector1.Size, vector2.Size);
+            for (int i = 0; i < border; i++)
             {
                 result += vector1.components[i] * vector2.components[i];
-                i++;
             }
             return result;
         }
